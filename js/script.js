152699377a9e9f929561c61d3aba74892bdda448
Aalind Sharma <aalind.01@gmail.com>
2019-05-21 15:58:55 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var channelName = 'TechGuyWeb';
+
+$(document).ready(function(){
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels", {
+            part: 'contentDetails',
+            forUsername: channelName,
+            key: 'AIzaSyAksqndsJ-IizEeldUx3CH-4PsQ5hLzLEg'
+        },
+        function (data){
+            $.each( data.items, function(idx, item){
+                console.log(item);
+                pid = item.contentDetails.relatedPlaylists.uploads;
+                getVids(pid);
+            })
+        }
+    )
+
+    function getVids(pid){
+        $.get(
+            "https://www.googleapis.com/youtube/v3/playlistItems", {
+                part: 'snippet',
+                maxResults: 10,
+                playlistId: pid,
+                key: 'AIzaSyAksqndsJ-IizEeldUx3CH-4PsQ5hLzLEg'
+            },
+            function (data){
+                var output;
+                $.each( data.items, function(idx, item){
+                    console.log(item);
+                    videoTitle = item.snippet.title;
+                    videoId = item.snippet.resourceId.videoId;
+                    output = '<li><iframe src=\"//www.youtube.com/embed/'+videoId+'\'"></iframe></li>';
+                    $('#results').append(output);
+                })
+            }
+        )  
+    }
+});
